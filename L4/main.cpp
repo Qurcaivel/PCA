60534a511ccd095ba2d1e1b30b5d365cc0b5f48d
Inqognitoo <alexinq6@gmail.com>
2023-05-24 00:00:02 +0300

    Update L4
    Changes:
    - new_time_init serve async task now
    - printf in irq hd seems to corrupt program in vbox dos
    - puts in irq hd prints correctly but stops program

--- L4/main.cpp
+++ L4/main.cpp
@@ -13,14 +13,14 @@ typedef unsigned char byte;
 
 struct datetime {
     byte wday, mday, month, year;
-    byte secs, mins, hours;
+    byte hours, mins, secs;
 };
 
 // Constants 
 
 const unsigned int datetime_registers[] = {
     0x06, 0x07, 0x08, 0x09, // wday, mday, month, year
-    0x00, 0x02, 0x04,       // secs, mins, hours
+    0x04, 0x02, 0x00,       // secs, mins, hours
 };
 
 const char* const month[] = {
@@ -42,7 +42,9 @@ const char* const week[] = {
 
 // Global var state
 
-int time_repeats;
+unsigned int time_running;
+unsigned int time_repeats;
+unsigned int exec_repeats;
 
 // Interrupt handlers
 
@@ -115,30 +117,51 @@ void input_datetime(struct datetime* dt)
 
 // Clock control functions
 
-void set_delay(int ms)
+void set_delay(unsigned int ms)
 {
     _disable();
 
     old_time_int = getvect(0x70);
     setvect(0x70, new_time_int);
 
-    _enable();
-
     outp(0xA1, inp(0xA1) & 0xFE); // unmask request signal line
     outp(0x70, 0xB);
     outp(0x71, inp(0x71) | 0x40); // period interrupt
 
+    time_running = 1;
     time_repeats = 0;
-    while(time_repeats <= ms){}   // wait expirancy | todo: не до пус ти мо!!! в тз написано, без зависаний, так и работаем, братва, вызывай хандл в инте
+    exec_repeats = ms;
+
+    _enable();
+}
+
+void reset_delay()
+{
+    _disable();
+
+    outp(0xA1, inp(0xA1) | 0x01); // mask irq
+    outp(0x70, 0xB);
+    outp(0x71, inp(0x71) & 0xBF); // period interrupt disabe
+
     setvect(0x70, old_time_int);
+
+    _enable();
 }
 
 void interrupt new_time_int(...)
 {
     time_repeats++;
+    
     outp(0x70, 0x0C); // select memory in CMOS
     inp(0x71);
     
+    if(time_repeats == exec_repeats)
+    {
+        reset_delay();
+        time_running = 0;
+        puts("Delay has been expired\n");
+    }
+
     outp(0x20, 0x20); // send End Of Interrupt
     outp(0xA0, 0x20);
 }
@@ -147,9 +170,42 @@ void interrupt new_time_int(...)
 
 int main()
 {
-    printf("Wow");
-    set_delay(2000);
-    printf("Owo");
+    unsigned int ms;
+    int option;
+    struct datetime dt;
 
-    return 0;
+    printf("Menu:\n\t1 - print datetime\n\t2 - input datetime\n\t3 - start delay\n\t4 - exit program\n\n");
+
+    for(;;)
+    {
+        scanf("%d", &option);
+
+        switch(option)
+        {
+            case 1:
+                get_datetime(&dt);
+                print_datetime(&dt);
+                break;
+
+            case 2:
+                input_datetime(&dt);
+                set_datetime(&dt);
+                break;
+
+            case 3:
+                if(time_running){
+                    printf("Error: timer in use\n");
+                }
+                scanf("%u", &ms);
+                set_delay(ms);
+                break;
+                
+            case 4:
+                if(time_running){
+                    printf("Exit: reset running delay\n");
+                    reset_delay();
+                }
+                return 0;
+        }
+    }
 }
